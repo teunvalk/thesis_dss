28a368fb17ae1413b6d74004b5d8b33abb82a42d
teunvalk <103424016+teunvalk@users.noreply.github.com>
2022-05-20 14:42:00 +0200

    Add files via upload

--- Reporting and plots per dataset.py
+++ Reporting and plots per dataset.py
@@ -296,7 +296,7 @@ plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.legend(bbox_to_anchor=(1.01,1))
+plt.legend(bbox_to_anchor=(1.01,1),prop={'size': 20})
 
 # Save plot
 plt.savefig('Figures/cell level ROC zheng.png', dpi=300)
@@ -566,7 +566,7 @@ plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.legend(bbox_to_anchor=(1.01,1))
+plt.legend(bbox_to_anchor=(1.01,1),prop={'size': 20})
 
 # Save plot
 plt.savefig('Figures/cell level ROC baron_human.png', dpi=300)
@@ -836,7 +836,7 @@ plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.legend(bbox_to_anchor=(1.01,1))
+plt.legend(bbox_to_anchor=(1.01,1),prop={'size': 20})
 
 # Save plot
 plt.savefig('Figures/cell level ROC inter.png', dpi=300)
